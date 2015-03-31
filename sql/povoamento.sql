3b08c7aacd8e2c16aa8e7938ff557b2e67dab35a
Mazuh <marcell-mz@hotmail.com>
2015-03-30 23:40:24 -0300

    Criação de tables melhorada. Povoamento inicial feito.

---
+++ sql/povoamento.sql
@@ -0,0 +1,23 @@
+﻿-- TABELA hierarquias ----------------------
+INSERT INTO hierarquias(nome) VALUES('admin');
+INSERT INTO hierarquias(nome) VALUES('instrutor');
+INSERT INTO hierarquias(nome) VALUES('aluno');
+
+-- TABELA usuarios --------------------------
+-- administradores
+INSERT INTO usuarios(id, id_hierarquia, nome, senha)
+	VALUES(90001, 1, 'Ale', 'senhaultrasecreta');
+
+-- instrutores
+INSERT INTO usuarios(id, id_hierarquia, nome, senha)
+	VALUES(90002, 2, 'Luciana', 'pinguelo');
+	
+INSERT INTO usuarios(id, id_hierarquia, nome, senha)
+	VALUES(90003, 2, 'Mayara', 'bolotascheias');
+	
+INSERT INTO usuarios(id, id_hierarquia, nome, senha)
+	VALUES(90004, 2, 'Marcell', 'ecoando');
+
+-- aluno de testes
+INSERT INTO usuarios(id, id_hierarquia, nome, senha)
+	VALUES(0, 3, 'Marcell', '1');
--- sql/tabelas.sql
+++ sql/tabelas.sql
@@ -1,36 +1,36 @@
 ﻿CREATE TABLE hierarquias(
 	id SERIAL PRIMARY KEY,
-	nome VARCHAR(10)
+	nome VARCHAR(10) NOT NULL
 );
 
 CREATE TABLE usuarios(
 	id SERIAL PRIMARY KEY,
-	id_categoria INTEGER
+	id_hierarquia INTEGER NOT NULL
 		REFERENCES hierarquias(id),
-	nome VARCHAR(50) NOT NULL,
+	nome VARCHAR(50) NOT NULL DEFAULT 'Aluno',
 	senha VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE cursos(
 	id SERIAL PRIMARY KEY,
-	nome VARCHAR(50),
-	qtd_exercicios INTEGER,
-	url_gabarito VARCHAR(50),
+	nome VARCHAR(50) NOT NULL,
+	qtd_exercicios INTEGER NOT NULL,
+	url_gabarito VARCHAR(50) NOT NULL,
 	url_gabarito_alt VARCHAR(50)
 );
 
 CREATE TABLE exercicios(
 	id SERIAL PRIMARY KEY,
-	id_aluno INTEGER
+	id_aluno INTEGER NOT NULL
 		REFERENCES usuarios(id),
-	id_curso INTEGER
+	id_curso INTEGER NOT NULL
 		REFERENCES cursos(id),
-	id_instrutor INTEGER
+	id_instrutor INTEGER NOT NULL
 		REFERENCES usuarios(id),
-	num_aula INTEGER,
-	qtd_perguntas INTEGER,
-	respostas TEXT,
-	dt_envio VARCHAR(8),
+	num_aula INTEGER NOT NULL,
+	qtd_perguntas INTEGER NOT NULL,
+	respostas TEXT NOT NULL,
+	dt_envio VARCHAR(8) DEFAULT '00/00/00',
 	correcao TEXT,
-	is_corrigido SMALLINT
+	is_corrigido SMALLINT NOT NULL DEFAULT 0
 );
