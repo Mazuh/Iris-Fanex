68c85c08ff189ccfa7a2135d5b0c7ef39b3dfee6
Mazuh <marcell-mz@hotmail.com>
2015-04-18 11:27:31 -0300

    Implementada listagem de cursos.

--- src/br/com/fanex/mazuh/janelas/instrutores/Cursos_CRUD.java
+++ src/br/com/fanex/mazuh/janelas/instrutores/Cursos_CRUD.java
@@ -23,6 +23,11 @@
  */
 package br.com.fanex.mazuh.janelas.instrutores;
 
+import br.com.fanex.mazuh.acesso.Sessao;
+import br.com.fanex.mazuh.edu.Curso;
+import br.com.fanex.mazuh.jpa.CursoJpaController;
+import java.util.List;
+
 /**
  *
  * @author mazuh
@@ -30,6 +35,8 @@ package br.com.fanex.mazuh.janelas.instrutores;
 // CRU: create, read e update.
 public class Cursos_CRUD extends javax.swing.JFrame { 
 
+    private final int TB_ROWS_N = 15; // número de linhas da tabela
+    
     /**
      * Creates new form Cursos_CRU
      */
@@ -39,8 +46,111 @@ public class Cursos_CRUD extends javax.swing.JFrame {
         this.setResizable(false);
         this.setTitle("Cursos");
         this.setLocationRelativeTo(null);
+        
+        // editando tamanho das colunas.
+        tbCursos.getColumnModel().getColumn(0).setMaxWidth(60); // id
+        
+        // só habilita o botão de deletar se for admin
+        btnDelete.setEnabled(Sessao.usuario_logado().getIdHierarquia().getNome().equalsIgnoreCase("admin"));
     }
 
+    /*
+    Atualiza a paginação e preenche a tabela de alunos.
+    */
+    private void atualizar(){
+        setPaginacaoParaTantosObjetos(getQtdCursos(), TB_ROWS_N);
+        preencherTbDeCursos();
+    }
+    
+    /*
+    Ajusta o limite de páginas de acordo com a quantidade de objetos
+    e seta no jLabel no formato "x/y", onde 'x' é resetado a 1 e 'y' é o limite.
+    */
+    private void setPaginacaoParaTantosObjetos(int qtdObjetos, int qtdLinhas){
+        int paginas;// novo limite de páginas
+
+        boolean isDivisivel = qtdObjetos % qtdLinhas == 0; // se qtd é divisível pela qtd de linhas
+        
+        // divide a quantidade entre as linhas disponíveis
+        paginas = (int) (qtdObjetos / qtdLinhas); // irá receber o menor inteiro do quociente da divisão
+        
+        paginas += (isDivisivel ? 0 : 1); // se o número não for divisĩvel, +1 pra compensar
+        
+        // pronto!
+        jPagina.setText("1/" + paginas);
+        
+    }
+    
+    private void preencherTbDeCursos(){
+        // índice para saber qual a primeira posição do bd a ser consultado
+        // é definido baseado na página atual da tabela
+        // ps: achando o número da página:
+        //          "x/y"   ->   split("/")   ->   [0]="x", [1]="y".
+        int first = (Integer.valueOf(jPagina.getText().split("/")[0]) - 1) * TB_ROWS_N;
+                                                  // Sendo tb_rows_n = 15, ex: 
+                                                  //     pag = 1 -> indice = 0
+                                                  //     pag = 2 -> indice = 15
+                                                  //     pag = 3 -> indice = 30 (...)
+        
+        // recupera as strings a serem colocadas na tabela baseada na query
+        String[][] registros = getRegistros(TB_ROWS_N, first);
+        
+        // escreve as strings na tabela
+        for (int row = 0; (row < registros.length && row < TB_ROWS_N); row++){
+            tbCursos.setValueAt(registros[row][0], row, 0);
+            tbCursos.setValueAt(registros[row][1], row, 1);
+        }
+        
+    }
+    
+    /*
+    Realiza uma consulta limitada entre os usuários do sistema e retorna
+    apenas um vetor de textos referentes a estas consultadas
+    */
+    private String[][] getRegistros(int max, int first){
+        // vars 
+        String[][] registros = new String[max][2]; // vai receber as linhas para retornar
+        List<Curso> cursos; // armazenar-se-á todos os obj consultados 
+        
+        // data access object
+        CursoJpaController cursoDAO = new CursoJpaController(Sessao.getEntityManagerFactory());
+        
+        // busca limitada (buscar por todos causaria prejuízo de desempenho)
+        cursos = cursoDAO.findCursoEntities(max, first);
+        
+        for (int i = 0; (i < cursos.size() && i < max); i++){
+            String txtId; // a se adicionado no array 
+            String txtCurso;
+            
+            Curso curso = (cursos.get(i)); // acesso mais rápido ao curso do índice 
+            
+            // com id, nome e quantidade de aulas
+            txtCurso = (curso.getNome() + " [" + curso.getQtdExercicios() + " aulas]");
+            txtId = String.valueOf(curso.getId());
+            
+            // adiciona a linha depois de ter os dados filtrados
+            try {
+                registros[i][0] = txtId;
+                registros[i][1] = txtCurso;
+                
+            } catch (NullPointerException e) {
+                 System.err.println("Erro no getter de registro.\n"
+                         + "Tentativa falha de armazenar no vetor.");
+            }
+            
+        } // fim do laço for
+        
+        return registros;
+        
+    }
+    
+    /*
+    Retorna a quantidade de linhas de usuários no banco de dados
+    */
+    private int getQtdCursos(){
+        return new CursoJpaController(Sessao.getEntityManagerFactory()).getCursoCount();
+    }
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -52,13 +162,23 @@ public class Cursos_CRUD extends javax.swing.JFrame {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         tbCursos = new javax.swing.JTable();
-        btnUpdate = new javax.swing.JButton();
+        btnEdit = new javax.swing.JButton();
         btnDelete = new javax.swing.JButton();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        btnAdd = new javax.swing.JButton();
+        btnBack = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
+        btnAnt = new javax.swing.JButton();
+        btnProx = new javax.swing.JButton();
+        jPagina = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        addWindowFocusListener(new java.awt.event.WindowFocusListener() {
+            public void windowGainedFocus(java.awt.event.WindowEvent evt) {
+                formWindowGainedFocus(evt);
+            }
+            public void windowLostFocus(java.awt.event.WindowEvent evt) {
+            }
+        });
 
         tbCursos.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -103,25 +223,42 @@ public class Cursos_CRUD extends javax.swing.JFrame {
         tbCursos.getColumnModel().getSelectionModel().setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
         if (tbCursos.getColumnModel().getColumnCount() > 0) {
             tbCursos.getColumnModel().getColumn(0).setResizable(false);
+            tbCursos.getColumnModel().getColumn(1).setResizable(false);
         }
 
-        btnUpdate.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/com/fanex/mazuh/janelas/imgs/icon/lapis.gif"))); // NOI18N
-        btnUpdate.setText("Modificar");
+        btnEdit.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/com/fanex/mazuh/janelas/imgs/icon/lapis.gif"))); // NOI18N
+        btnEdit.setText("Modificar");
 
         btnDelete.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/com/fanex/mazuh/janelas/imgs/icon/lixo.gif"))); // NOI18N
         btnDelete.setText("Deletar");
 
-        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/com/fanex/mazuh/janelas/imgs/icon/mais.gif"))); // NOI18N
-        jButton1.setText("Adicionar novo curso");
+        btnAdd.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/com/fanex/mazuh/janelas/imgs/icon/mais.gif"))); // NOI18N
+        btnAdd.setText("Adicionar novo curso");
 
-        jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/com/fanex/mazuh/janelas/imgs/icon/janela.gif"))); // NOI18N
-        jButton2.setText("Voltar ao paInel");
+        btnBack.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/com/fanex/mazuh/janelas/imgs/icon/janela.gif"))); // NOI18N
+        btnBack.setText("Voltar ao paInel");
 
         jLabel2.setBackground(new java.awt.Color(39, 174, 96));
         jLabel2.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
         jLabel2.setForeground(new java.awt.Color(255, 255, 255));
         jLabel2.setText("LISTA DE CURSOS DISPONÍVEIS PARA OS ALUNOS");
         jLabel2.setOpaque(true);
+
+        btnAnt.setText("<");
+        btnAnt.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAntActionPerformed(evt);
+            }
+        });
+
+        btnProx.setText(">");
+        btnProx.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnProxActionPerformed(evt);
+            }
+        });
+
+        jPagina.setText("1/1");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -135,11 +272,17 @@ public class Cursos_CRUD extends javax.swing.JFrame {
                         .addGap(0, 0, Short.MAX_VALUE)
                         .addComponent(jLabel2)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jButton2))
+                        .addComponent(btnBack))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jButton1)
+                        .addComponent(btnAdd)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(btnUpdate)
+                        .addComponent(btnAnt)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jPagina)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnProx)
+                        .addGap(43, 43, 43)
+                        .addComponent(btnEdit)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(btnDelete)))
                 .addContainerGap())
@@ -150,12 +293,15 @@ public class Cursos_CRUD extends javax.swing.JFrame {
                 .addGap(7, 7, 7)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel2)
-                    .addComponent(jButton2))
+                    .addComponent(btnBack))
                 .addGap(11, 11, 11)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(btnUpdate)
+                    .addComponent(btnEdit)
                     .addComponent(btnDelete)
-                    .addComponent(jButton1))
+                    .addComponent(btnAdd)
+                    .addComponent(btnAnt)
+                    .addComponent(btnProx)
+                    .addComponent(jPagina))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 275, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
@@ -163,6 +309,44 @@ public class Cursos_CRUD extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    /*
+    Atualiza tudo quando ganha foco. A paginação e a tabela resetadas.
+    */
+    private void formWindowGainedFocus(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowGainedFocus
+        atualizar();
+    }//GEN-LAST:event_formWindowGainedFocus
+
+    private void btnAntActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAntActionPerformed
+                // encontra página
+        String[] paginacao = jPagina.getText().split("/"); // var auxiliar para split 
+        int pagina = Integer.valueOf(paginacao[0]);
+        
+        // se não tiver no limite, pode retroceder 
+        if (pagina > 1)
+            pagina--;
+        
+        // seta nova página
+        jPagina.setText(pagina + "/" + paginacao[1]);
+        
+        preencherTbDeCursos();
+    }//GEN-LAST:event_btnAntActionPerformed
+
+    private void btnProxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnProxActionPerformed
+        // encontra página e limite de páginas 
+        String[] paginacao = jPagina.getText().split("/"); // var auxiliar para split 
+        int pagina = Integer.valueOf(paginacao[0]);
+        int limite = Integer.valueOf(paginacao[1]);
+        
+        // se não tiver no limite, pode avançar 
+        if (pagina < limite)
+            pagina++;
+        
+        // seta nova página
+        jPagina.setText(pagina + "/" + limite);
+        
+        preencherTbDeCursos();
+    }//GEN-LAST:event_btnProxActionPerformed
 
     /**
      * @param args the command line arguments
@@ -201,11 +385,14 @@ public class Cursos_CRUD extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnAdd;
+    private javax.swing.JButton btnAnt;
+    private javax.swing.JButton btnBack;
     private javax.swing.JButton btnDelete;
-    private javax.swing.JButton btnUpdate;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
+    private javax.swing.JButton btnEdit;
+    private javax.swing.JButton btnProx;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jPagina;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable tbCursos;
     // End of variables declaration//GEN-END:variables
